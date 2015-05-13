43a757316e18e42c01020c5dfc2399d5cb294d27
Youmin Ha <youmin.ha@samsung.com>
2015-05-13 15:54:51 +0900

    Block switching to same user as an user occupying current seat
    Switching to same user as the current user raises a abnormal behavior,
    failing on re-login process. This is due to the inconsistance between
    the tlm-sessiond daemon lifecycle and the systemd-logind's logout
    procedure.
    The standard solution is to receive a sessionTerminated signal from the
    systemd-logind and sync it to the re-login procedure of TLM, but it
    will take many structure changes of tlm-sessiond.
    This patch just blocks switching to same user, preventing the abnormal
    case with just a small effort.
    
     * src/daemon/tlm-seat.c|h: tlm_seat_get_occupying_username() is added.
     * src/daemon/tlm-dbus-observer.c: _is_valid_switch_user_dbus_request()
       is added and used in _process_request().
    
    In addition, several comments and debug logs are given.
    
    Change-Id: I9238098771bcce2b40aa89da47e9391ff7556922

--- src/daemon/tlm-seat.h
+++ src/daemon/tlm-seat.h
@@ -67,6 +67,13 @@ tlm_seat_new (TlmConfig *config,
 const gchar *
 tlm_seat_get_id (TlmSeat *seat);
 
+/** Get the username who occupies the seat
+ * @return  The name of the user who holds the seat
+ * @return  NULL if nobody occupies the seat
+ */
+const gchar *
+tlm_seat_get_occupying_username (TlmSeat* seat);
+
 gboolean
 tlm_seat_switch_user (TlmSeat *seat,
                       const gchar *service,
@@ -76,7 +83,7 @@ tlm_seat_switch_user (TlmSeat *seat,
 
 gboolean
 tlm_seat_create_session (TlmSeat *seat,
-                         const gchar *service, 
+                         const gchar *service,
                          const gchar *username,
                          const gchar *password,
                          GHashTable *environment);
